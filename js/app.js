c2906ec7c29dd4c1fa6731502c17796764f4e50f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-01-31 20:40:57 -0500

    added api for hourly forecast for up to 40 hours

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ console.log("It's working!")
 $(() => {
     console.log($)
     $.ajax({
-        url:"https://api.openweathermap.org/data/2.5/weather?zip=98125,us&units=imperial&APPID=d8ef3b8d18705a7d1fa4cf253411f06b"
+        url:"https://api.openweathermap.org/data/2.5/weather?zip=11746,us&units=imperial&APPID=d8ef3b8d18705a7d1fa4cf253411f06b"
     }).then( data => {
         console.log(data);
         let icon = $("<img>").attr("src", `img/${data.weather[0].icon}@2x.png`).attr("id", "current-icon");
@@ -14,6 +14,19 @@ $(() => {
         $("#current-weather").append(icon);
         $("#current-weather").append(temperature);
         $("#current-weather").append(feelsLikeTemp);
+    });
+
+    $.ajax({
+        url:"http://api.openweathermap.org/data/2.5/forecast?zip=11746,us&units=imperial&APPID=d8ef3b8d18705a7d1fa4cf253411f06b"
+    }).then( data => {
+        console.log(data);
+        for(let i = 0; i < data.list.length; i++) {
+            let icon = $("<img>").attr("src", `img/${data.list[i].weather[0].icon}@2x.png`);
+            let $div = $("<div>").addClass("hour-card")
+            let $p = $("<p>").text(`${Math.ceil(data.list[i].main.temp)}`);
+            $div.append(icon, $p);
+            $("#hourly-weather").append($div);
+        }
     });
 
 });
